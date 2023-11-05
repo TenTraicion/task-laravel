6ee270947e0c45b9f5e71de449926e96946e6f6c
TenTraicion <tentraicion@gmail.com>
2023-11-05 14:14:19 +0600

    Update task-begin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
-Test Index Page
+<h1>
+  The list of tasks
+</h1>
 
-@isset($name)  
-  The Name of the Framework: {{ $name }}
-@endisset
+<div>
+  Hello Im a blade template!
+  {{-- @if (count($tasks)) --}}
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
+    <div>There are no tasks!</div>
+  @endforelse
+  {{-- @endif --}}
+</div>
