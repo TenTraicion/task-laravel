922cf1077a8002e080a34193288420cbc19eeba6
TenTraicion <tentraicion@gmail.com>
2023-11-10 22:17:41 +0600

    Flash Msg

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,21 +2,39 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+<style>
+  .error-message{
+    color: red;
+    font-size: 0, 8rem;
+  }
+</style>
+@endsection
+
 @section('content')
-{{ $errors }}
+{{-- {{ $errors }} --}}
   <form method="POST" action="{{ route('tasks.store') }}">
     @csrf
     <div>
       <label for="title">Title</label>
       <input type="text" name="title" id="title">
+      @error('title')
+        <p class="error-message">{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <label for="desc">Description</label>
       <textarea name="desc" id="desc" cols="15" rows="5"></textarea>
+      @error('desc')
+        <p class="error-message">{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <label for="ldesc">Long Description</label>
       <textarea name="ldesc" id="ldesc" cols="30" rows="10"></textarea>
+      @error('ldesc')
+        <p class="error-message">{{ $message }}</p>
+      @enderror
     </div>
     <div>
       <button type="submit">Add Task</button>
