7cfbcd9d87125221481f8d9104f739f3831c1b0a
TenTraicion <tentraicion@gmail.com>
2023-11-10 22:02:21 +0600

    Post and Store

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 // use Illuminate\Http\Response;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get(),
+        'tasks' => Task::latest()->get(),
     ]); 
 })->name('tasks.index');
 
@@ -29,12 +30,25 @@ Route::view('/tasks/create', 'create')->name('tasks.create');
 Route::get('/tasks/{id}', function ($id) {
     
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id),
+        'task' => Task::findOrFail($id),
     ]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    // dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'desc' => 'required',
+        'ldesc' => 'required',
+    ]);
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['desc'];
+    $task->long_description = $data['ldesc'];
+    
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id'=> $task->id])->with('success','Data Added Successfully!');
 })->name('tasks.store');
 
 Route::get('/laravel', function () {
